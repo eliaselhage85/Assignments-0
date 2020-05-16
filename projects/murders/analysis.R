7180c265226728f98d3214ca158fe372ec01f480
eliaselhage85 <eliaselhage85@gmail.com>
2020-05-16 15:41:54 -0400

    Testing

--- projects/murders/analysis.R
+++ projects/murders/analysis.R
@@ -4,3 +4,4 @@ load("rda/murders.ra")
 murders%>%mutate(abb=reorder(abb,rate))%>%
   ggplot(aes(abb,rate))  +geom_bar(width=0.5,stat="identity",color="black")+coord_flip()
 ggsave("figs/barplot.png")
+
